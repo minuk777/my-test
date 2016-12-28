7d888f008601acea83bea5ae18fb92862a855dfb
minuk777 <kcabemoc@naver.com>
2016-12-28 14:40:54 +0900

    poll
    poll

--- PollingTest.js
+++ PollingTest.js
@@ -14,6 +14,8 @@ class PollingTest extends React.Component {
 		this.state = {
 			count:0,
 			name:"first",
+			ball:"./loading_ball1.gif",
+			ball2:"./loading_ball2.gif"
 		};
 	}
 
@@ -24,7 +26,10 @@ class PollingTest extends React.Component {
 
 	render() {
 		return(
-			<div>{this.state.count}:{this.state.name}<ReactInterval timeout={3000} enabled={true} callback={this.pollThis.bind(this)} /></div>
+			<div style={{backgroundColor:"yellow"}}>{this.state.count}:{this.state.name}
+			<img src={this.state.ball} /><img src={this.state.ball2} />
+			<ReactInterval timeout={3000} enabled={true} callback={this.pollThis.bind(this)} />
+			</div>
 		);
 	}
 }
